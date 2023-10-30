646d48ba4215d87e041b9104eade9d0373792cae
asimshah <asimsshah377@gmail.com>
2023-10-30 23:58:04 +0500

    video 6 fix responses

--- api/routes/product.js
+++ api/routes/product.js
@@ -9,8 +9,21 @@ router.get("/", (req, res, next) => {
   Product.find()
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {     //product response in array and count it 
+        count: docs.length,
+        products: docs.map(doc =>{
+          return{
+            name: doc.name,
+            price:doc.price,
+            _id:doc._id,
+            request:{
+              type: 'Get',
+              url:'http://localhost:3000/product/'+ doc._id
+            }
+          }
+        })
+      } 
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -31,8 +44,15 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(200).json({
-        message: "handling post request to /product",
-        createdProduct: product,
+        message: "product create successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request:{
+            type: 'Get',
+            url:'http://localhost:3000/product/'+ result._id
+        }},
       });
     })
     .catch((err) => {
@@ -46,11 +66,17 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+  .select('name price _id')
     .exec()
     .then((doc) => {
       console.log(doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+        Product:doc,
+        request:{
+          type: 'GET',
+          url: 'http://localhost:3000/product'
+        }})
       } else {
         res.status(404).json({ message: "No value found in db" });
       }
@@ -70,8 +96,13 @@ router.patch("/:productId", (req, res, next) => {
   product.updateOne({_id: id}, { $set: updateOps})
   .exec()
   .then(result => {
-    console.log(result);
-    res.status(200).json(result)
+    res.status(200).json({
+      message: 'product updated',
+      request:{
+        type: 'GET',
+        url: 'http://localhost:3000/product'+ id
+      }
+    })
   })
   .catch(err => {
     console.log(err);
@@ -86,7 +117,14 @@ router.delete("/:productId", (req, res, next) => {
   Product.deleteOne({ _id: id })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: 'Product Deleted',
+        request:{
+          type: 'POST',
+          url:'http://localhost:3000/product',
+          body: {name: 'string',price: 'Number'}
+        }
+      });
     })
     .catch((err) => {
       console.log(err);
