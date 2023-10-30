6900f1777d902e0d22004cb49fdcc645fb84dab6
asimshah <asimsshah377@gmail.com>
2023-10-30 20:55:48 +0500

    video-6-crud in mongodb

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,47 +1,99 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
+const product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "handling get request to /product",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
-   const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-   }
-  res.status(200).json({
-    message: "handling post request to /product",
-    createdProduct: product
   });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "handling post request to /product",
+        createdProduct: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-router.get('/:productId',(req, res, next)=>{
+router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if(id === 'special'){
-    res.status(200).json({
-      message:'you discovered the special ID',
-      id: id
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: "No value found in db" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  }else{
-    res.status(200).json({
-      message:'you passad ID'
-    });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for(const ops of req.body){
+    updateOps[ops.propName] = ops.value;
   }
-});
-
-router.patch('/:productId',(req, res, next)=>{
-  res.status(200).json({
-    message:'update ID'
+  product.updateOne({_id: id}, { $set: updateOps})
+  .exec()
+  .then(result => {
+    console.log(result);
+    res.status(200).json(result)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
   })
 });
 
-router.delete('/:productId',(req, res, next)=>{
-  res.status(200).json({
-    message:'deleted ID'
-  })
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,49 +1,56 @@
 const express = require("express");
 const app = express(); //just execute like a function
-const morgan = require('morgan');
-const bodyParser = require('body-parser'); // handle to request body parms
+const morgan = require("morgan");
+const bodyParser = require("body-parser"); // handle to request body parms
+const mongoose = require("mongoose");
+
 
 const productRoutes = require("./api/routes/product");
 const orderRoutes = require("./api/routes/order");
+// const product = require("./api/models/product");
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(
+  "mongodb+srv://node-shop:node-shop@node-rest-shop.yabwjjz.mongodb.net/?retryWrites=true&w=majority"
+); 
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //header to allow access to the multiple clients
-app.use((req, res, next)=>{
-    res.header("Access-control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Request-with, Content-Type, Accept, Authorization"
-    );
-    if(res.method === 'OPITION'){
-        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-    next();
-})
+app.use((req, res, next) => {
+  res.header("Access-control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (res.method === "OPITIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //"use()" is used for middleware
 // Routes which handle the requests
-app.use('/product', productRoutes);
-app.use('/order', orderRoutes);
+app.use("/product", productRoutes);
+app.use("/order", orderRoutes);
 
 // if user hit unknown path
-app.use((req,res,next)=>{
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
 //if response get some error
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
