6b28da452514629fcd121c6238e74da2d0b247c1
asimshah <asimsshah377@gmail.com>
2023-11-20 16:09:32 +0500

    video 13 adding jwt route protection

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -3,10 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req,res,next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        console.log('token',token)
-        console.log(req.userData)
         const decoded = jwt.verify(token,'foo')
-        console.log('decoded',decoded)
         req.userData = decoded
         next();
     }catch(error){
